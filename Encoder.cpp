f4f59cb6f30b436885f7bc5f263190262c47c6ee
Pavithran Pathmarajah <pavithranpath@gmail.com>
2016-10-22 20:51:21 -0400

    intial Modules

---
+++ Encoder.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Pavi on 2016-10-22.
+//
+
+#include "Encoder.h"
+
+char* Encoder::encodeString(char* input){
+    return 'E'+'n'+'c'+'o'+'d'+'e'+'d'+' '+input;
+}
---
+++ Setup.h
@@ -0,0 +1,15 @@
+//
+// Created by Pavi on 2016-10-22.
+//
+
+#ifndef TMP_SETUP_H
+#define TMP_SETUP_H
+
+
+class Setup {
+public:
+    int setUp();
+};
+
+
+#endif //TMP_SETUP_H
